13d32db84eddb8cbcf1c559a784e3e0692651554
Vitalii M. <mail@inkwalker.xyz>
2024-11-04 17:52:42 +0100

    Booh

--- booh/TemplateData/modal.js
+++ booh/TemplateData/modal.js
@@ -3,18 +3,30 @@
         dialog: null,
         form: null,
         data: null,
+        isDialogOpen: false,
         openDialog: function () {
             const dialog = this.dialog;
             dialog.showModal();
+            this.isDialogOpen = true;
         },
         init: function () {
             this.dialog = document.querySelector('#airdrop-modal');
             this.form = document.querySelector('#airdrop-form');
             this.form.addEventListener('submit', this.handleSubmit.bind(this));
+            this.dialog.addEventListener('click', this.dismissDialog.bind(this));
         },
+
         closeDialog: function () {
             const dialog = this.dialog;
             dialog.close();
+            this.isDialogOpen = false;
+        },
+        dismissDialog: function (event) {
+            const dialogContent = document.querySelector('.modal-content');
+            if (!this.isDialogOpen) return;
+            if (!dialogContent.contains(event.target)) {
+                this.closeDialog();
+            }
         },
         handleSubmit: function (event) {
             event.preventDefault();
